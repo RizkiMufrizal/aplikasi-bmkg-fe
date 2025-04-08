518f95eb1394248aebd7aa845d7ebc0574b10d00
RizkiMufrizal <mufrizalrizki@gmail.com>
2025-04-08 09:51:42 +0700

    update all

--- web/js/app.js
+++ web/js/app.js
@@ -1,8 +1,13 @@
-angular.module('app', [])
-    .controller('app.controller', function($scope, $http) {
-
-        $http.get('https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json')
-            .then(function(response){
-                $scope.dataGempaBumi = response.data;
-            });
+angular.module("app", []).controller("app.controller", function ($scope, $http) {
+    $http.get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json").then(function (response) {
+        $scope.dataGempaBumi = response.data;
     });
+
+    $http
+        .get("https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=11.07.09.2009")
+        .then(function (response) {
+            $scope.dataCuaca = response.data.data[0].cuaca[0];
+
+            console.log($scope.dataCuaca);
+        });
+});
